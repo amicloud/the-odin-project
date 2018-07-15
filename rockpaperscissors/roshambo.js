5f4efe2baf8995f9bf265f31c28605e3a8e22b83
amicloud <wsmithsrs@gmail.com>
2018-07-15 16:49:13 -0700

    Completed DOM Manipulation exercise. Added a UI with buttons, a log, scores, and a popup. eh

--- rockpaperscissors/roshambo.js
+++ rockpaperscissors/roshambo.js
@@ -2,6 +2,108 @@ console.log('js loaded');
 let playerScore = 0;
 let computerScore = 0;
 let currentRound = 0;
+reset();
+
+
+function playRound(button, computerSelection) {
+    let playerSelection = button.id;
+    switch (playerSelection) {
+        case 'rock':
+            switch (computerSelection) {
+                case 'rock':
+                    handleRoundResults('Draw', null, null);
+                    break;
+                case 'scissors':
+                    handleRoundResults('Player', 'rock', 'scissors');
+                    break;
+                case 'paper':
+                    handleRoundResults('Computer', 'paper', 'rock');
+                    break
+            }
+            break;
+        case 'scissors':
+            switch (computerSelection) {
+                case 'rock':
+                    handleRoundResults('Computer', 'scissors', 'rock');
+                    break;
+                case 'scissors':
+                    handleRoundResults('Draw', null, null);
+                    break;
+                case 'paper':
+                    handleRoundResults('Player', 'scissors', 'paper');
+                    break
+            }
+            break;
+        case 'paper':
+            switch (computerSelection) {
+                case 'rock':
+                    handleRoundResults('Player', 'paper', 'rock');
+                    break;
+                case 'scissors':
+                    handleRoundResults('Computer', 'scissors', 'paper');
+                    break;
+                case 'paper':
+                    handleRoundResults('Draw', null, null);
+                    break
+            }
+            break;
+    }
+}
+
+function handleRoundResults(winner, winnerMove, loserMove){
+    currentRound++;
+    switch(winner){
+        case 'Player':
+            playerScore++;
+            break;
+        case 'Computer':
+            computerScore++;
+            break;
+        case 'Draw':
+            break;
+    }
+    let text;
+    if(winner === 'Draw'){
+        text = `R ${currentRound}: Draw`;
+    } else {
+        text = `R ${currentRound}: ${winner} wins; ${winnerMove} beats ${loserMove}`;
+    }
+    postTextToLog(text);
+    updatePlayer1Score();
+    updatePlayer2Score();
+    checkForWinner();
+}
+
+function updatePlayer1Score(){
+    document.getElementById('player-score').textContent = playerScore.toString();
+}
+function updatePlayer2Score(){
+    document.getElementById('computer-score').textContent = computerScore.toString();
+}
+
+function checkForWinner() {
+    if(playerScore >= 5){
+        postTextToLog('Player Won!');
+        document.getElementById('winner').textContent = 'Player Won!';
+        pop('popDiv');
+
+    } else if (computerScore >= 5){
+        postTextToLog('Computer Won!');
+        document.getElementById('winner').textContent = 'Computer Won!';
+        pop('popDiv');
+    }
+}
+
+function postTextToLog(text){
+    const div = document.createElement('div');
+    div.textContent = text;
+    div.classList.add('game-log-item');
+    const log = document.getElementById('game-log');
+    log.appendChild(div);
+    if(log.children.length > 10){
+        log.removeChild(log.children[0]);
+    }
+}
 
 function createEventListeners(){
     let rock = document.getElementById('rock');
@@ -30,44 +132,16 @@ function computerPlay() {
     }
 }
 
-function appendRoundResults(winner, winnerMove, loserMove){
-    currentRound++;
-    switch(winner){
-        case 'player':
-            playerScore++;
-            break;
-        case 'computer':
-            computerScore++;
-            break;
-        case 'draw':
-            break;
-    }
-    let text;
-    if(winner === 'draw'){
-        text = `R${currentRound}: This one was a draw...`;
-    } else {
-        text = `R${currentRound} Winner is: ${winner}; ${winnerMove} beats ${loserMove}`;
-    }
-    const div = document.createElement('div');
-    div.textContent = text;
-    div.classList.add('game-log-item');
-    const log = document.getElementById('game-log');
-    log.appendChild(div);
-    if(log.children.length > 10){
-        log.removeChild(log.children[0]);
-    }
-    document.getElementById('player-score').textContent = playerScore.toString();
-    document.getElementById('computer-score').textContent = computerScore.toString();
-}
-
 function reset() {
     playerScore = 0;
     computerScore = 0;
     currentRound = 0;
 
-    removeAllChildElements('game-log');
-    document.getElementById(playerScore).textContent = '0';
-    document.getElementById(computerScore).textContent = '0';
+    if(document.getElementById('game-log').hasChildNodes()){
+        removeAllChildElements('game-log');
+    }
+    document.getElementById('player-score').textContent = '0';
+    document.getElementById('computer-score').textContent = '0';
 }
 
 function removeAllChildElements(element){
@@ -77,62 +151,9 @@ function removeAllChildElements(element){
     }
 }
 
-function playRound(button, computerSelection) {
-    let playerSelection = button.id;
-    switch (playerSelection) {
-        case 'rock':
-            switch (computerSelection) {
-                case 'rock':
-                    appendRoundResults('draw', null, null);
-                    break;
-                case 'scissors':
-                    appendRoundResults('player', 'rock', 'scissors');
-                    break;
-                case 'paper':
-                    appendRoundResults('computer', 'paper', 'rock');
-                    break
-            }
-            break;
-        case 'scissors':
-            switch (computerSelection) {
-                case 'rock':
-                    appendRoundResults('computer', 'scissors', 'rock');
-                    break;
-                case 'scissors':
-                    appendRoundResults('draw', null, null);
-                    break;
-                case 'paper':
-                    appendRoundResults('player', 'scissors', 'paper');
-                    break
-            }
-            break;
-        case 'paper':
-            switch (computerSelection) {
-                case 'rock':
-                    appendRoundResults('player', 'paper', 'rock');
-                    break;
-                case 'scissors':
-                    appendRoundResults('computer', 'scissors', 'paper');
-                    break;
-                case 'paper':
-                    appendRoundResults('draw', null, null);
-                    break
-            }
-            break;
-    }
-
-    function checkForWinner() {
-        if(playerScore >= 5){
-            prompt('Player Won!');
-            reset();
-        } else if (computerScore >= 5){
-            prompt('Computer Won!');
-            reset();
-        }
-    }
-
-    checkForWinner();
+function pop(div) {
+    document.getElementById(div).style.display = 'block';
 }
-
-
-//TODO: Stop checkForWinner() from preventing the scores resetting
+function hide(div) {
+    document.getElementById(div).style.display = 'none';
+}
