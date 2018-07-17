adc7814a2000deb030505c8af1f64a6880899cbf
amicloud <wsmithsrs@gmail.com>
2018-07-16 21:28:52 -0700

    JS Exercise - snakeCase completed.
    This one took a while. I wanted a short and concise solution so it's a regex fest.

--- javascript-exercises-master/snakeCase/snakeCase.js
+++ javascript-exercises-master/snakeCase/snakeCase.js
@@ -1,5 +1,18 @@
-var snakeCase = function() {
+let snakeCase = function(q) {
+    let phrase = q;
+    //Replace all special characters and duplicate underscores
+    phrase = phrase.replace(specialCharacters, '_').replace(/_{2,}/g, "_");
+    //Handles camelCase
+    if(phrase.match(/[a-z][A-Z]/) && !phrase.match(/[_]/)){
+        phrase = phrase.replace(/[A-Z]/g, '_$&')
+    }
+    //Removes any trailing underscores, which could be the result of trailing punctuation, whitespace, etc.
+    if(phrase[phrase.length-1] === '_'){
+        phrase = phrase.substr(0, phrase.length-1);
+    }
+    return phrase.toLowerCase();
+};
+module.exports = snakeCase;
 
-}
-
-module.exports = snakeCase
+//Generated RegEx
+const specialCharacters = new RegExp(/[ -#%-/:-@[-`{-~\u00a0-¡¦-©«-¬®-±´¶-¸»¿×÷˂-˅˒-˟˥-˫˭˯-\u036f͵;΄-΅·϶҂-\u0489՚-՟։-֊\u0591-\u05c7׳-״؆-؊،-؛؞-؟\u064b-\u065e٪-٭\u0670۔\u06d6-\u06dc\u06de-\u06e4\u06e7-\u06ed۽-۾܀-܍\u0711\u0730-\u074a\u07a6-\u07b0\u07eb-\u07f3߶-߹\u0901-\u0903\u093c\u093e-\u094d\u0951-\u0954\u0962-॥॰\u0981-\u0983\u09bc\u09be-\u09c4\u09c7-\u09c8\u09cb-\u09cd\u09d7\u09e2-\u09e3৺\u0a01-\u0a03\u0a3c\u0a3e-\u0a42\u0a47-\u0a48\u0a4b-\u0a4d\u0a51\u0a70-\u0a71\u0a75\u0a81-\u0a83\u0abc\u0abe-\u0ac5\u0ac7-\u0ac9\u0acb-\u0acd\u0ae2-\u0ae3\u0b01-\u0b03\u0b3c\u0b3e-\u0b44\u0b47-\u0b48\u0b4b-\u0b4d\u0b56-\u0b57\u0b62-\u0b63୰\u0b82\u0bbe-\u0bc2\u0bc6-\u0bc8\u0bca-\u0bcd\u0bd7௳-௸௺\u0c01-\u0c03\u0c3e-\u0c44\u0c46-\u0c48\u0c4a-\u0c4d\u0c55-\u0c56\u0c62-\u0c63౿\u0c82-\u0c83\u0cbc\u0cbe-\u0cc4\u0cc6-\u0cc8\u0cca-\u0ccd\u0cd5-\u0cd6\u0ce2-\u0ce3ೱ-ೲ\u0d02-\u0d03\u0d3e-\u0d44\u0d46-\u0d48\u0d4a-\u0d4d\u0d57\u0d62-\u0d63൹\u0d82-\u0d83\u0dca\u0dcf-\u0dd4\u0dd6\u0dd8-\u0ddf\u0df2-෴\u0e31\u0e34-\u0e3a\u0e47-๏๚-๛\u0eb1\u0eb4-\u0eb9\u0ebb-\u0ebc\u0ec8-\u0ecd༁-༟༴-\u0f3f\u0f71-\u0f87\u0f90-\u0f97\u0f99-\u0fbc྾-࿌࿎-࿔\u102b-\u103e၊-၏\u1056-\u1059\u105e-\u1060\u1062-\u1064\u1067-\u106d\u1071-\u1074\u1082-\u108d\u108f႞-႟჻\u135f-፨᎐-᎙᙭-᙮\u1680᚛-᚜᛫-᛭\u1712-\u1714\u1732-᜶\u1752-\u1753\u1772-\u1773\u17b6-៖៘-៚\u17dd᠀-\u180e\u18a9\u1920-\u192b\u1930-\u193b᥀᥄-᥅\u19b0-\u19c0\u19c8-\u19c9᧞-᧿\u1a17-\u1a1b᨞-᨟\u1b00-\u1b04\u1b34-\u1b44᭚-᭼\u1b80-\u1b82\u1ba1-\u1baa\u1c24-\u1c37᰻-᰿᱾-᱿\u1dc0-\u1de6\u1dfe-\u1dff᾽᾿-῁῍-῏῝-῟῭-`´-῾\u2000-\u200a‐-\u2029\u202f-\u205f⁺-⁾₊-₎\u20d0-\u20f0℀-℁℃-℆℈-℉℔№-℘℞-℣℥℧℩℮℺-℻⅀-⅄⅊-⅍⅏←-⏧␀-␦⑀-⑊⒜-ⓩ─-⚝⚠-⚼⛀-⛃✁-✄✆-✉✌-✧✩-❋❍❏-❒❖❘-❞❡-❵➔➘-➯➱-➾⟀-⟊⟌⟐-⭌⭐-⭔⳥-⳪⳹-⳼⳾-⳿\u2de0-⸮⸰⺀-⺙⺛-⻳⼀-⿕⿰-⿻\u3000-〄〈-〠\u302a-〰〶-〷〽-〿\u3099-゜゠・㆐-㆑㆖-㆟㇀-㇣㈀-㈞㈪-㉃㉐㉠-㉿㊊-㊰㋀-㋾㌀-㏿䷀-䷿꒐-꓆꘍-꘏\ua66f-꙳\ua67c-꙾꜀-꜖꜠-꜡꞉-꞊\ua802\ua806\ua80b\ua823-꠫꡴-꡷\ua880-\ua881\ua8b4-\ua8c4꣎-꣏\ua926-꤯\ua947-\ua953꥟\uaa29-\uaa36\uaa43\uaa4c-\uaa4d꩜-꩟\ufb1e﬩﴾-﴿﷽\ufe00-︙\ufe20-\ufe26︰-﹒﹔-﹦﹨﹪-﹫！-＃％-／：-＠［-｀｛-･￢-￤￨-￮￼-�]|\ud800[\udd00-\udd02\udd37-\udd3f\udd79-\udd89\udd90-\udd9b\uddd0-\uddfd\udf9f\udfd0]|\ud802[\udd1f\udd3f\ude01-\ude03\ude05-\ude06\ude0c-\ude0f\ude38-\ude3a\ude3f\ude50-\ude58]|\ud809[\udc70-\udc73]|\ud834[\udc00-\udcf5\udd00-\udd26\udd29-\udd72\udd7b-\udddd\ude00-\ude45\udf00-\udf56]|\ud835[\udec1\udedb\udefb\udf15\udf35\udf4f\udf6f\udf89\udfa9\udfc3]|\ud83c[\udc00-\udc2b\udc30-\udc93]|\udb40[\udd00-\uddef]|\ud800[\udd00-\udd01\uddfd\udf9f\udfd0]|\ud802[\udd1f\udd3f\ude01-\ude03\ude05-\ude06\ude0c-\ude0f\ude38-\ude3a\ude3f\ude50-\ude58]|\ud809[\udc70-\udc73]|\ud834[\udd65-\udd69\udd6d-\udd72\udd7b-\udd82\udd85-\udd8b\uddaa-\uddad\ude42-\ude44]|\udb40[\udd00-\uddef]/, 'g');
