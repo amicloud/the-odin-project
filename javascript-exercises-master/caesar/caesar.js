a76803dc6477bae03224761d6a57e607b7b3af7a
amicloud <wsmithsrs@gmail.com>
2018-07-17 02:25:17 -0700

    Javascript Exercise - Caesar done.
    Ended up being kinda golfy

--- javascript-exercises-master/caesar/caesar.js
+++ javascript-exercises-master/caesar/caesar.js
@@ -1,5 +1,13 @@
-var caesar = function() {
+let  caesar = function(str, shift) {
+    str = str.replace(/[A-Za-z]/g, (match) => {
+        const char = match.charCodeAt(0);
+        let offset = char >= 97 ? 97 : 65;
+        return String.fromCharCode(((char - offset + shift) % 26) + offset + (char - offset + shift < 0 ? 26 : 0))
+    });
+    return str;
+};
 
-}
 
-module.exports = caesar
+
+module.exports = caesar;
+
