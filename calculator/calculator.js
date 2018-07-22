7b82a9183e2c0e05d8a5d933b541cdc4c0eea041
amicloud <wsmithsrs@gmail.com>
2018-07-22 14:53:10 -0700

    Committing before major refactor...

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,45 +1,127 @@
 const buttonHolder = document.getElementById('button-holder');
 const historyContainer = document.getElementById('calculator-history');
-const display = document.getElementById('display');
+const displayX = document.getElementById('display-x');
+const displayY = document.getElementById('display-y');
+const displayFlag = document.getElementById('display-flag');
 const base = 10;
-const xRegisterSize = 16; //This is effectively the character limit of the display as well
+const xRegisterSize = 16; //This is effectively the character limit of the displayX-x as well
 let xReg = 0;
+let prevXReg = 0;
 let yReg = 0;
-updateXRegister(xReg);
+let prevYReg = 0;
+let flags = {};
+    flags.none = '';
+    flags.addition = '+';
+    flags.subtraction = '-';
+    flags.multiplication = 'x';
+    flags.division = '/';
+let currentFlag;
+let prevFlag = flags.none;
+clearAll();
+const unaryOperators = new UnaryOperators();
+const binaryOperators = new BinaryOperators();
+let clearOnNextNumber = false;
 
 const buttons = [
     new Button('%', 'f',  null, null),
-    new Button('√', 'f',  null, null),
-    new Button('x²', 'f', null, null),
-    new Button('¹/ₓ', 'f', null, null),
-    new Button('CE', 'f', null, null),
-    new Button('C', 'f',  null, null),
-    new Button('⌫', 'f', backspace, null),
+    new Button('√', 'f',  unaryOperators.squareRoot),
+    new Button('x²', 'f', unaryOperators.square),
+    new Button('¹/ₓ', 'f', unaryOperators.inverse),
+    new Button('CE', 'f', clearEntry),
+    new Button('C', 'f',  clearAll),
+    new Button('⌫', 'f', backspace),
     new Button('/', 'f',  null, null),
     new Button('7', 'n',  pushToXRegister, 7),
     new Button('8', 'n',  pushToXRegister, 8),
     new Button('9', 'n',  pushToXRegister, 9),
-    new Button('X', 'f',  null, null),
+    new Button('X', 'f',  null),
     new Button('4', 'n',  pushToXRegister, 4),
     new Button('5', 'n',  pushToXRegister, 5),
     new Button('6', 'n',  pushToXRegister, 6),
-    new Button('-', 'f',  null, null),
+    new Button('-', 'f',  null),
     new Button('1', 'n',  pushToXRegister, 1),
     new Button('2', 'n',  pushToXRegister, 2),
     new Button('3', 'n',  pushToXRegister, 3),
-    new Button('+', 'f',  null, null),
-    new Button('±', 'f',  null, null),
+    new Button('+', 'f',  binaryOperators.addition),
+    new Button('±', 'f',  unaryOperators.invertSign, null),
     new Button('0', 'n',  pushToXRegister, 0),
-    new Button('.', 'f',  null, null),
-    new Button('=', 'f',  null, null)
+    new Button('.', 'd',  null),
+    new Button('=', 'f',  binaryOperators.equals)
 ];
+
+function UnaryOperators(){
+    this.squareRoot = () => {
+      updateXRegister(Math.sqrt(xReg));
+    };
+    this.invertSign = () => {
+        updateXRegister(xReg * -1);
+    };
+    this.inverse = () => {
+        updateXRegister(1/xReg)
+    };
+    this.square = () => {
+        updateXRegister(Math.pow(xReg, 2));
+    };
+
+}
+
+function updateFlag(flag) {
+    currentFlag = flag;
+    displayFlag.innerHTML = currentFlag;
+}
+
+function BinaryOperators(){
+    this.addition = () => {
+        evaluate();
+        updateFlag(flags.addition);
+
+    };
+    this.equals = () => {
+        if(currentFlag === flags.none){
+            currentFlag = prevFlag;
+            updateYRegister(prevYReg)
+        }
+        prevFlag = currentFlag;
+        evaluate();
+    };
+    function evaluate(){
+        switch(currentFlag){
+            case flags.addition:
+                if(xReg === 0) updateXRegister(prevXReg);
+                prevXReg = xReg;
+                prevYReg = yReg;
+                updateXRegister(xReg + yReg);
+                updateYRegister(0);
+                updateFlag(flags.none);
+                break;
+            case flags.none:
+                updateYRegister(xReg);
+        }
+        clearOnNextNumber = true;
+    }
+}
 
 function backspace() {
     let x = parseInt(xReg.toString().slice(0, -1));
     updateXRegister(x ? x : 0);
 }
 
+function clearEntry(){
+    updateXRegister(0);
+}
+
+function clearAll(){
+    updateXRegister(0);
+    updateYRegister(0);
+    updateFlag(flags.none);
+    prevXReg = 0;
+}
+
 function pushToXRegister(val){
+    if(clearOnNextNumber) {
+        updateXRegister(0);
+        clearOnNextNumber = false;
+    }
     let newXreg = (xReg * base) + val;
     if(Math.log10(newXreg) >= xRegisterSize){
         console.log('Register overflow prevented');
@@ -52,14 +134,15 @@ function pushToXRegister(val){
 
 function updateXRegister(val){
     xReg = val;
-    display.innerHTML = val;
+    displayX.innerHTML = xReg.toString();
 }
 
-function pushToYRegister(val){
-    yReg = (yReg * base) + val;
+function updateYRegister(val){
+    yReg = val;
+    displayY.innerHTML = yReg ? yReg.toString() : '';
 }
 
-function Button(label, type, func, val) {
+function Button(label, type, func, val = null) {
     let that = this;
     this.label = label;
     this.val = val;
@@ -98,7 +181,6 @@ for(let r = 0; r < 6 ; r++) {
     for (let i = 0; i < 4; i++) {
         let btn = buttons[(r * 4) + i];
         let button = createDisplayButton(btn);
-
         row.appendChild(button);
     }
     buttonHolder.appendChild(row);
